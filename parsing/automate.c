8a618be65c7f543f8c005026b39e7abaec0ab13b
Sinthujan Sitchsapashan <ssitchsa@paul-f3Ar1s2.clusters.42paris.fr>
2024-06-19 21:20:45 +0200

    automate_start

---
+++ parsing/automate.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   automate.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssitchsa <ssitchsa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/19 21:10:49 by ssitchsa          #+#    #+#             */
+/*   Updated: 2024/06/19 21:20:13 by ssitchsa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+// ft init qu'on appelle dans le main 
+
+/*
+
+bool init(t_token *token, t_minishell *minishell)
+{
+	if (!token)
+		return (1);
+	if (token->type == WORD)
+		return (word_state(token->next, minishell));
+	if (token->type == REDIR)
+		return (redir_state(...));
+	else
+		return (0);
+}
+
+
+bool word_state
+
+
+*/
