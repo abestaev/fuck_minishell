87fc3c8754c8e900d0e9782a34f8f6fb0845dd20
abestaev <gatsby.hewitt@gmail.com>
2024-09-02 18:20:37 +0200

    add redirection single in

--- exec/redirection.c
+++ exec/redirection.c
@@ -6,7 +6,7 @@
 /*   By: albestae <albestae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 21:23:59 by albestae          #+#    #+#             */
-/*   Updated: 2024/09/02 18:04:20 by albestae         ###   ########.fr       */
+/*   Updated: 2024/09/02 18:07:51 by albestae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,25 @@ int	get_outfile(t_command *command)
 	return (EXIT_SUCCESS);
 }
 
+int get_infile(t_command *command)
+{
+	int fd;
+	fd = open(command->redirections->file, O_RDONLY);
+	if (fd == -1)
+	{
+		perror("open failed\n");
+		return (EXIT_FAILURE);
+	}
+	if (fd > 0 && dup2(fd, STDIN_FILENO) < 0)
+	{
+		perror("dup2 failed\n");
+		close(fd);
+		return (EXIT_FAILURE);
+	}
+	close(fd);
+	return (EXIT_SUCCESS);	
+}
+
 int	get_redir(t_command *command)
 {
 	t_command	*tmp;
@@ -57,8 +76,8 @@ int	get_redir(t_command *command)
 		if (command->redirections->type == SINGLE_OUT
 			|| command->redirections->type == DOUBLE_OUT)
 			get_outfile(command);
-		// else if (command->redirections->type == SINGLE_IN)
-		// get_infile(command, minishell);
+		else if (command->redirections->type == SINGLE_IN)
+			get_infile(command);
 		command->redirections = command->redirections->next;
 	}
 	command = tmp;
